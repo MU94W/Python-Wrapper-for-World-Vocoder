0d8a7f0a4937c89b2d0fc0d7fd401519dc2db300
Wang Mu <nanqiao15@126.com>
2017-09-19 18:34:22 +0800

    add 2 demo

--- demo/demo.py
+++ demo/demo.py
@@ -57,7 +57,8 @@ def main(args):
         rmtree('test')
     os.mkdir('test')
 
-    x, fs = sf.read('utterance/vaiueo2d.wav')
+    #x, fs = sf.read('utterance/vaiueo2d.wav')
+    x, fs = sf.read('utterance/p226_002.wav')
     # x, fs = librosa.load('utterance/vaiueo2d.wav', dtype=np.float64)
 
     # 1. A convient way
@@ -93,11 +94,42 @@ def main(args):
     # librosa.output.write_wav('test/y_harvest_with_f0_refinement.wav', y_h, fs)
     sf.write('test/y_harvest_with_f0_refinement.wav', y_h, fs)
 
+    # 2-4 DIO with F0 refinement (using Stonemask). Code and restore sp, ap.
+    code_sp = pw.code_spectral_envelope(sp, fs, 80)
+    code_ap = pw.code_aperiodicity(ap, fs)
+    fft_size = (sp.shape[1] - 1) * 2
+    rest_sp = pw.decode_spectral_envelope(code_sp, fs, fft_size)
+    rest_ap = pw.decode_aperiodicity(code_ap, fs, fft_size)
+    y_r = pw.synthesize(f0, rest_sp, rest_ap, fs, args.frame_period)
+    sf.write('test/y_with_f0_refinement_code_and_restore.wav', y_r, fs)
+    print("fft size: {:d}".format(fft_size))
+    print("coded sp shape: ({:d}, {:d})".format(code_sp.shape[0], code_sp.shape[1]))
+    print("coded ap shape: ({:d}, {:d})".format(code_ap.shape[0], code_ap.shape[1]))
+
+    # 2-5 DIO with F0 refinement (using Stonemask). Code and restore sp, ap. frame_shift: 12.5 ms, frame_length: 50.0 ms
+    f0_xx, t_xx = pw.dio(x, fs, f0_floor=50.0, f0_ceil=600.0,
+                         channels_in_octave=2,
+                         frame_period=12.5,
+                         speed=args.speed)
+    f0_xx = pw.stonemask(x, f0_xx, t_xx, fs)
+    sp_xx = pw.cheaptrick(x, f0_xx, t_xx, fs)
+    ap_xx = pw.d4c(x, f0_xx, t_xx, fs)
+    code_sp_xx = pw.code_spectral_envelope(sp_xx, fs, 80)
+    code_ap_xx = pw.code_aperiodicity(ap_xx, fs)
+    fft_size = (sp_xx.shape[1] - 1) * 2
+    rest_sp_xx = pw.decode_spectral_envelope(code_sp_xx, fs, fft_size)
+    rest_ap_xx = pw.decode_aperiodicity(code_ap_xx, fs, fft_size)
+    y_r_xx = pw.synthesize(f0_xx, rest_sp_xx, rest_ap_xx, fs, 12.5)
+    sf.write('test/y_with_f0_refinement_code_and_restore_frame_period_12.5.wav', y_r_xx, fs)
+    print("coded sp_xx shape: ({:d}, {:d})".format(code_sp_xx.shape[0], code_sp_xx.shape[1]))
+    print("coded ap_xx shape: ({:d}, {:d})".format(code_ap_xx.shape[0], code_ap_xx.shape[1]))
+
+
     # Comparison
-    savefig('test/wavform.png', [x, _y, y])
-    savefig('test/sp.png', [_sp, sp])
-    savefig('test/ap.png', [_ap, ap], log=False)
-    savefig('test/f0.png', [_f0, f0])
+    savefig('test/wavform.png', [x, _y, y, y_h, y_r, y_r_xx])
+    savefig('test/sp.png', [_sp, sp, sp_h, rest_sp, rest_sp_xx])
+    savefig('test/ap.png', [_ap, ap, ap_h, rest_ap, rest_ap_xx], log=False)
+    savefig('test/f0.png', [_f0, f0, f0_h, f0_xx])
 
     print('Please check "test" directory for output files')
 
